a4fc00299235de3c7036a36a0ad0ffbdd9d14476
Alexander Azakevich <alexander.azar97@gmail.com>
2022-06-02 14:13:12 +0300

    Создал метод findMinNumb, который принимает спискок чисел (List number), для нахождения минимального числа из списка. Изменил диапозон чисел Random от -20 до 20 Обьявил тело метода по поиску минимального числа. Вывод на экран сделал более интерактивным.

--- src/FindMinFromList.java
+++ src/FindMinFromList.java
@@ -7,10 +7,23 @@ public class FindMinFromList {
         List<Integer> numbers = new ArrayList<>(20);
         Random getRandomNumbers = new Random();
         for (int i = 0; i < 20; i++) {
-            numbers.add(i, getRandomNumbers.nextInt(50) - 10);
+            numbers.add(i, getRandomNumbers.nextInt(40) -20);
         }
         for (int i = 0; i < 20; i++) {
-            System.out.println(numbers.get(i));
+            System.out.println(i + " element list = " + numbers.get(i));
         }
+        System.out.println();
+        System.out.println("Minimal number from List = " + findMinNumb(numbers));
+
+    }
+
+    public static int findMinNumb(List numbers) {
+        int min = (int) numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (min > (int) numbers.get(i)) {
+                min = (int) numbers.get(i);
+            }
+        }
+        return min;
     }
 }
