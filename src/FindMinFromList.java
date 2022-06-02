e80dbcf67005549dc048bd65a1b9bf4b224d2d65
Alexander Azakevich <alexander.azar97@gmail.com>
2022-06-02 13:35:04 +0300

    Создал список типа Integer с название numbers

--- src/FindMinFromList.java
+++ src/FindMinFromList.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindMinFromList {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+    }
 
 }
