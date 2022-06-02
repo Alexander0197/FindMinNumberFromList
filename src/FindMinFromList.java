6f448a5ad63e1759690b727892344434efc74c28
Alexander Azakevich <alexander.azar97@gmail.com>
2022-06-02 13:53:03 +0300

    Создал экземпляр класса Random. Заполнил лист случайными числами в диапозоне. Вывел лист на экран. для проверки заполнения.

--- src/FindMinFromList.java
+++ src/FindMinFromList.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class FindMinFromList {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>(20);
+        Random getRandomNumbers = new Random();
+        for (int i = 0; i < 20; i++) {
+            numbers.add(i, getRandomNumbers.nextInt(50) - 10);
+        }
+        for (int i = 0; i < 20; i++) {
+            System.out.println(numbers.get(i));
+        }
     }
-
 }
